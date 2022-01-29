e6f0c302411781ab08e70a901bc116f7bf961148
Anastasiia-Tarasiuk <93443287+Anastasiia-Tarasiuk@users.noreply.github.com>
2022-01-29 15:51:53 +0000

    Deploying to gh-pages from @ Anastasiia-Tarasiuk/ice-cream-page@6786025aa7a7abffa7a6355e303c7c749ba5e13a 🚀

--- src.dafee0ff.js
+++ src.dafee0ff.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream-page/src.6c91a29e.js.map
+//# sourceMappingURL=/ice-cream-page/src.dafee0ff.js.map
